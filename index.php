3dc26e473e40547d67178481ed3485baf760f5dc
yfwz100 <yfwz100@yeah.net>
2013-01-09 00:29:30 +0800

    Adapt to the changes in page user.php .

--- index.php
+++ index.php
@@ -4,5 +4,9 @@ session_start();
 
 require_once 'core/autoload.base.php';
 
+if (!file_exists('core/admin.config.php')) {
+	header("Location: " . gtf\Router::site('user.php'));
+}
+
 gtf\Router::dispatchOrExit('LoginController');
 gtf\Router::dispatch('DormLogController');
